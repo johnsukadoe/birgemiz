c93a928d7b7424f18f1dc75a5f480083709eacf0
Miras <bking4234@gmail.com>
2023-05-08 19:57:59 +0600

    11

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 document.getElementById('burger').addEventListener("click", function(){
     document.querySelector("header").classList.toggle("open");
 });
 
-=======
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById('burger').addEventListener("click", function(){
-        document.querySelector("header").classList.toggle("open");
-    });
-});
->>>>>>> origin/main
--- pages/memorial/script.js
+++ pages/memorial/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById('burger').addEventListener("click", function(){
     document.querySelector("header").classList.toggle("open");
 });
@@ -20,24 +19,3 @@ function closeModal(event){
         modal.style.display='none';
     }
 }
-=======
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById('burger').addEventListener("click", function(){
-        document.querySelector("header").classList.toggle("open");
-    });
-
-    
-
-});
-const openPopUp = document.getElementById("cityy");
-const closePopUp = document.getElementById("popUp-close");
-const popUp = document.getElementById("complexes-in-city");
-openPopUp.addEventListener('click', function(e){
-    e.preventDefault();
-    popUp.classList.add('openPopUp');
-});
-
-closePopUp.addEventListener('click', function(){
-    popUp.classList.remove('openPopUp');
-});
->>>>>>> origin/main
